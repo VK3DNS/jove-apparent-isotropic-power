975980b1e24394c99014e58c44bb90df852574b0
Nathan Scott <sco0006@jmss.vic.edu.au>
2024-05-23 11:13:09 +1000

    generalise comments from sun-specific descriptions

--- main.py
+++ main.py
@@ -5,6 +5,6 @@ c = Calculator()
 Hz = 2.01*10**7 #base freq. (Hz)
 mG = 9.3*10**0 #antenna maxgain (unit not specified, I assume dB)
 B = 6*10**3 #bandwidth (Hz)
-K = 1.41034*10**6 #antenna temp. (K)
-AuD = 9.94111*10**-1 #distance to the sun (AU)
+K = 9.1*10**4 #antenna temp. (K)
+AuD = 4.2*10**0 #distance to object (AU) ##Jupiter is about 4.2 AU
 print(f'apparent isotropic power transmitted over the 2MHz band is {c.isotropicpower(Hz,mG,B,K,AuD)} Joules')
