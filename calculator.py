6b8061d41eefa9c64e0cc8a57f9f60e7d8fe22a4
Nathan Scott <sco0006@jmss.vic.edu.au>
2024-05-30 22:06:59 +1000

    Added documentation for the functions. And some other small bug fixes probably, I forgot

--- calculator.py
+++ calculator.py
@@ -1,6 +1,18 @@
 from PySide6.QtCore import QObject, Signal, Slot
 
 class Sender(QObject):
+    """
+    class Sender:
+    Send values to the calculator.
+
+    Attributes:
+        receive_values: Signal
+        receive_Hz: Signal
+        receive_mG: Signal
+        receive_B: Signal
+        receive_K: Signal
+        receive_AuD: Signal
+    """
     receive_values = Signal(list)
     receive_Hz = Signal(float)
     receive_mG = Signal(float)
@@ -9,6 +21,36 @@ class Sender(QObject):
     receive_AuD = Signal(float)
 
 class Calculator(QObject):
+    """
+    class Calculator(QObject):
+    Calculate the isotropic power.
+
+    Attributes:
+        pi: float
+        C: float
+        kB: float
+        Hz: float
+        mG: float
+        B: float
+        K: float
+        AuD: float
+        isotropicpower: float
+        tx: instance of class Sender
+
+    Methods:
+        values(self)
+        get_Hz(self)
+        get_mG(self)
+        get_B(self)
+        get_K(self)
+        get_AuD(self)
+        set_Hz(self, Hz)
+        set_mG(self, mG)
+        set_B(self, B)
+        set_K(self, K)
+        set_AuD(self, AuD)
+        calculateisotropicpower(self)
+    """
     def __init__(self, tx, Hz = None, mG =None, B = None, K = None, AuD = None, *args):
         from math import pi
         self.pi: float = pi
@@ -29,60 +71,194 @@ class Calculator(QObject):
 
     @Slot()
     def values(self):
+        """
+        @Slot()
+        values(self)
+        Send the values to the calculator.
+
+        Key lines:
+            self.tx.receive_values.emit([self.Hz, self.mG, self.B, self.K, self.AuD])
+        """
         self.tx.receive_values.emit([self.Hz, self.mG, self.B, self.K, self.AuD])
 
     @Slot()
     def get_Hz(self):
+        """
+        @Slot()
+        get_Hz(self)
+        Send the Hz value to the calculator.
+
+        Key lines:
+            self.tx.receive_Hz.emit(self.Hz)
+        """
         self.tx.receive_Hz.emit(self.Hz)
 
     @Slot()
     def get_mG(self):
+        """
+        @Slot()
+        get_mG(self)
+        Send the mG value to the calculator.
+
+        Key lines:
+            self.tx.receive_mG.emit(self.mG)
+        """
         self.tx.receive_mG.emit(self.mG)
 
     @Slot()
     def get_B(self):
+        """
+        @Slot()
+        get_B(self)
+        Send the B value to the calculator.
+
+        Key lines:
+            self.tx.receive_B.emit(self.B)
+        """
         self.tx.receive_B.emit(self.B)
 
     @Slot()
     def get_K(self):
+        """
+        @Slot()
+        get_K(self)
+        Send the K value to the calculator.
+
+        Key lines:
+            self.tx.receive_K.emit(self.K)
+        """
         self.tx.receive_K.emit(self.K)
 
     @Slot()
     def get_AuD(self):
+        """
+        @Slot()
+        get_AuD(self)
+        Send the AuD value to the calculator.
+
+        Key lines:
+            self.tx.receive_AuD.emit(self.AuD)
+        """
         self.tx.receive_AuD.emit(self.AuD)
 
     @Slot(float)
     def set_Hz(self, Hz):
+        """
+        @Slot(float)
+        set_Hz(self, Hz)
+        Set the Hz value to the calculator.
+        Update the isotropic power.
+
+        Parameters:
+            Hz: float
+            The value of Hz from the calculator.
+
+        Key lines:
+            self.Hz = float(Hz)
+            self.calculateisotropicpower()
+            self.tx.receive_Hz.emit(self.Hz)
+        """
         self.Hz = float(Hz)
         self.calculateisotropicpower()
         self.tx.receive_Hz.emit(self.Hz)
 
     @Slot(float)
     def set_mG(self, mG):
+        """
+        @Slot(float)
+        set_mG(self, mG)
+        Set the mG value to the calculator.
+        Update the isotropic power.
+
+        Parameters:
+            mG: float
+            The value of mG from the calculator.
+
+        Key lines:
+            self.mG = float(mG)
+            self.calculateisotropicpower()
+            self.tx.receive_mG.emit(self.mG)
+        """
         self.mG = float(mG)
         self.calculateisotropicpower()
         self.tx.receive_mG.emit(self.mG)
 
     @Slot(float)
     def set_B(self, B):
+        """
+        @Slot(float)
+        set_B(self, B)
+        Set the B value to the calculator.
+        Update the isotropic power.
+
+        Parameters:
+            B: float
+            The value of B from the calculator.
+
+        Key lines:
+            self.B = float(B)
+            self.calculateisotropicpower()
+            self.tx.receive_B.emit(self.B)
+        """
         self.B = float(B)
         self.calculateisotropicpower()
         self.tx.receive_B.emit(self.B)
 
 
     @Slot(float)
-    def set_K(self, K, *args):
+    def set_K(self, K):
+        """
+        @Slot(float)
+        set_K(self, K)
+        Set the K value to the calculator.
+        Update the isotropic power.
+
+        Parameters:
+            K: float
+            The value of K from the calculator.
+
+        Key lines:
+            self.K = float(K)
+            self.calculateisotropicpower()
+            self.tx.receive_K.emit(self.K)
+        """
         self.K = float(K)
         self.calculateisotropicpower()
         self.tx.receive_K.emit(self.K)
 
     @Slot(float)
     def set_AuD(self, AuD):
+        """
+        @Slot(float)
+        set_AuD(self, AuD)
+        Set the AuD value to the calculator.
+        Update the isotropic power.
+
+        Parameters:
+            AuD: float
+            The value of AuD from the calculator.
+
+        Key lines:
+            self.AuD = float(AuD)
+            self.calculateisotropicpower()
+            self.tx.receive_AuD.emit(self.AuD)
+        """
         self.AuD = float(AuD)
         self.calculateisotropicpower()
         self.tx.receive_AuD.emit(self.AuD)
 
     def calculateisotropicpower(self, *args):
+        """
+        calculateisotropicpower(self, *args)
+        Calculate the isotropic power.
+
+        Key lines:
+            self.isotropicpower = iW*2*10**6
+
+        Returns:
+            self.isotropicpower: float
+            outdated return. not used in the current version of the code.
+        """
         λ: float = self.C/self.Hz  # wavelength
         G: float = 10**(self.mG/10)  # antenna gain
         Ae: float = G*λ**2/(4*self.pi)  # effective area
@@ -99,7 +275,21 @@ class Calculator(QObject):
         return self.isotropicpower
 
 def main():
-    c = Calculator()
+    """
+    main()
+    Test the Calculator class.
+
+    Local Variables:
+        c: instance of class Calculator
+
+    Key lines:
+        c = Calculator()
+        print(c.isotropicpower)
+
+    idk why you'd want to run calculator.py as a standalone script, but here you go. :)
+    """
+    print(main.__doc__)
+    c = Calculator(Sender())
     print(c.isotropicpower)
 
 
--- handler.py
+++ handler.py
@@ -25,32 +25,138 @@ class Communicate(QObject):
 
 
 class Receive:
+    """
+    class Receive:
+    Receive values from the calculator and store them in the values dictionary.
+    PySide6 does not allow a return value from a slot, so seperate signals are used to send the values to the handler.
+
+    Attributes:
+        None
+
+    Methods:
+        receive_values(self, array_values)
+        receive_Hz(self, Hz)
+        receive_mG(self, mG)
+        receive_B(self, B)
+        receive_K(self, K)
+        receive_AuD(self, AuD)
+    """
+
     @Slot(list)
     def receive_values(self, array_values):
+        """
+        @Slot(list)
+        receive_values(self, array_values)
+        Receive the values from the calculator and store them in the values dictionary.
+
+        Parameters:
+            array_values: list
+            The values from the calculator.
+
+        Key lines:
+            values[key] = value
+        """
         for key, value in zip(values.keys(), array_values):
             values[key] = value
 
     @Slot(float)
     def receive_Hz(self, Hz):
+        """
+        @Slot(float)
+        receive_Hz(self, Hz)
+        Receive the value of Hz from the calculator and store it in the values dictionary.
+
+        Parameters:
+            Hz: float
+            The value of Hz from the calculator.
+
+        Key lines:
+            values['Hz'] = Hz
+        """
         values['Hz'] = Hz
 
     @Slot(float)
     def receive_mG(self, mG):
+        """
+        @Slot(float)
+        receive_mG(self, mG)
+        Receive the value of mG from the calculator and store it in the values dictionary.
+
+        Parameters:
+            mG: float
+            The value of mG from the calculator.
+
+        Key lines:
+            values['mG'] = mG
+        """
         values['mG'] = mG
 
     @Slot(float)
     def receive_B(self, B):
+        """
+        @Slot(float)
+        receive_B(self, B)
+        Receive the value of B from the calculator and store it in the values dictionary.
+
+        Parameters:
+            B: float
+            The value of B from the calculator.
+
+        Key lines:
+            values['B'] = B
+        """
         values['B'] = B
 
     @Slot(float)
     def receive_K(self, K):
+        """
+        @Slot(float)
+        receive_K(self, K)
+        Receive the value of K from the calculator and store it in the values dictionary.
+
+        Parameters:
+            K: float
+            The value of K from the calculator.
+
+        Key lines:
+            values['K'] = K
+        """
         values['K'] = K
 
     @Slot(float)
     def receive_AuD(self, AuD):
+        """
+        @Slot(float)
+        receive_AuD(self, AuD)
+        Receive the value of AuD from the calculator and store it in the values dictionary.
+
+        Parameters:
+            AuD: float
+            The value of AuD from the calculator.
+
+        Key lines:
+            values['AuD'] = AuD
+        """
         values['AuD'] = AuD
 
 def run():
+    """
+    run()
+    Package the calculator, sender, receiver, and communicator into a single function. Useful for ease of reading in the main function.
+
+    Local Variables:
+        communicate: instance of class Communicate
+        rx: instance of class Receive
+        tx: instance of class Sender
+        calc: instance of class Calculator
+
+    Returns:
+        communicate: instance of class Communicate
+        values: dictionary
+        rx: instance of class Receive
+        tx: instance of class Sender
+        calc: instance of class Calculator
+    """
     from calculator import Calculator, Sender
 
 
@@ -81,36 +187,30 @@ def run():
     communicate.get_values.connect(calc.values)
     communicate.get_values.emit()
 
-
-
     return communicate, values, rx, tx, calc
 
 def main():
-    communicate = Communicate()
-    rx = Receive()
-    tx = Sender()
-    calc = Calculator(tx)
+    """
+    main()
+    Used for testing.
 
-    communicate.set_Hz.connect(calc.set_Hz)
-    communicate.set_mG.connect(calc.set_mG)
-    communicate.set_B.connect(calc.set_B)
-    communicate.set_K.connect(calc.set_K)
-    communicate.set_AuD.connect(calc.set_AuD)
+    Local Variables:
+        communicate: instance of class Communicate
+        values: dictionary
+        rx: instance of class Receive
+        tx: instance of class Sender
+        calc: instance of class Calculator
 
-    communicate.get_Hz.connect(calc.get_Hz)
-    communicate.get_mG.connect(calc.get_mG)
-    communicate.get_B.connect(calc.get_B)
-    communicate.get_K.connect(calc.get_K)
-    communicate.get_AuD.connect(calc.get_AuD)
+    Returns:
+        None
 
-    tx.receive_values.connect(rx.receive_values)
-    tx.receive_Hz.connect(rx.receive_Hz)
-    tx.receive_mG.connect(rx.receive_mG)
-    tx.receive_B.connect(rx.receive_B)
-    tx.receive_K.connect(rx.receive_K)
-    tx.receive_AuD.connect(rx.receive_AuD)
-
-    communicate.get_values.connect(calc.values)
+    Key lines:
+        communicate.get_values.emit()
+        communicate.set_K.emit(1)
+        print(f'K is {values["K"]}')
+        print(calc.isotropicpower)
+    """
+    communicate, values, rx, tx, calc = run()
     communicate.get_values.emit()
 
     print(f'K is {values["K"]}')
@@ -124,4 +224,5 @@ def main():
 
 
 if __name__ == '__main__':
+    print(Receive.receive_values.__doc__)
     main()
